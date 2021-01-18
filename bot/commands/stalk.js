6f5a6746e2af91579d5d6e3408b6598005765a20
sarmerer <sarmerer@gmail.com>
2021-01-18 20:09:16 +0500

    added flag support to stalk command

--- bot/commands/stalk.js
+++ bot/commands/stalk.js
@@ -1,17 +1,24 @@
 const { getUserFromMention, reply, respond } = require("../utils");
-const { default_throttle } = require("../config.json");
+const { prefix, default_throttle } = require("../config.json");
+const { modes } = require("../notify");
 const { log } = require("../logger");
 
 module.exports = {
   name: "stalk",
   description: "stalk user",
-  usage: "`//stalk <user> <?timeout>`",
+  usage: `${prefix}stalk @user <-t 30> <-m online | offline | all>`,
   args: true,
-  execute(message, args) {
+  execute(message, args, flags) {
     let member = getUserFromMention(message, args[0]);
     if (!member) return reply(message, `Usage: ${this.usage}`);
-    let debounce = Number.parseInt(args[1]);
-    if (args[1] && (!debounce || debounce < default_throttle))
+    const mode = flags["-m"];
+    if (mode && !modes.includes(mode))
+      return reply(
+        message,
+        `${mode} mode not found, available modes: ${modes.join(", ")}`
+      );
+    let debounce = Number.parseInt(flags["-t"]) || 30;
+    if (flags["-t"] && (!debounce || debounce < default_throttle))
       return reply(
         message,
         `timeout should be a number, and can't be less than ${default_throttle} seconds`
@@ -34,18 +41,24 @@ module.exports = {
       );
       return;
     }
+    let dnd = args.includes("dnd");
+    let d = new Date();
+    d.setMinutes(d.getMinutes() - 1);
     let newStalker = {
       id: message.author.id,
       target: target.id,
       guildID: message.guild.id,
-      noOffline: args[2] && args[2] === "noOffline",
-      last_notification: new Date(),
+      debounce: debounce,
+      mode: mode,
+      dnd: dnd,
+      last_notification: d,
     };
-    if (debounce) newStalker.debounce = debounce;
     global.db.get("stalkers").push(newStalker).write();
     respond(
       message,
-      `${message.author.username} now stalks ${target.username}`
+      `${message.author.username} now stalks ${target.username}. Mode: ${mode}${
+        dnd ? ", don't disturb mode in on" : ""
+      }`
     );
     log(
       `[${message.author.username}] has started stalking [${target.username}]`
